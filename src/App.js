69864d7b73902cd17180b2f9ca28038b497e1f39
Conary36 <cbeckford144@gmail.com>
2019-12-10 20:12:05 -0500

    uploaded correct npm-pkg

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React,{useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState("");
+
+  const handleChange = event => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log(name);
+  };
+
+  return (
+    <div className="App">
+      {console.log(name)}
+      <form>
+        <label>
+          Username:
+          <input type="text" onChange={event => handleChange(event)} />
+        </label>
+        <button onSubmit={() => handleSubmit()}>Submit!</button>
+      </form>
+    </div>
+  );
+}
+export default App;
