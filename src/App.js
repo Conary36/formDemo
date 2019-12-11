b827585fb1c34dd2c501eff14afd0de569b38656
Conary36 <cbeckford144@gmail.com>
2019-12-10 20:17:17 -0500

    taking in various input

--- src/App.js
+++ src/App.js
@@ -4,26 +4,39 @@ import './App.css';
 
 function App() {
   const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const handleChange = event => {
+  const handleNameChange = event => {
     setName(event.target.value);
   };
 
-  const handleSubmit = () => {
+  const handlePasswordChange = event => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
     console.log(name);
+    console.log(password);
   };
 
   return (
     <div className="App">
-      {console.log(name)}
-      <form>
+      {console.log({ name })}
+      {console.log({ password })}
+      <form onSubmit={event => handleSubmit(event)}>
         <label>
           Username:
-          <input type="text" onChange={event => handleChange(event)} />
+          <input type="text" onChange={event => handleNameChange(event)} />
         </label>
-        <button onSubmit={() => handleSubmit()}>Submit!</button>
+        <label>
+          Password:
+          <input type="text" onChange={event => handlePasswordChange(event)} />
+        </label>
+        <button>Submit!</button>
       </form>
     </div>
   );
 }
+
 export default App;
