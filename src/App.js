b62b0051bdde8df17c6e0cc6b1f9b808e6bfa177
Conary36 <cbeckford144@gmail.com>
2019-12-12 13:21:13 -0500

    added Yup validation

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
 import React,{useState} from 'react';
 import { withFormik, Form, Field } from "formik";
-import logo from './logo.svg';
+import * as Yup from "yup";
+// import logo from './logo.svg';
 import './App.css';
 
-function LoginForm() {
-  const [user, setUser] = useState({ username: "", password: "" });
+function LoginForm({errors, touched}) {
+  // const [user, setUser] = useState({ username: "", password: "" });
 
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+  // const handleChange = event => {
+  //   setUser({ ...user, [event.target.name]: event.target.value });
+  // };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.username);
-    console.log(user.password);
-  };
-
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   console.log(user.username);
+  //   console.log(user.password);
+  // };
+  
   return (
     <div className="loginForm">
-      {console.log(user)}
-      <form onSubmit={event => handleSubmit(event)}>
-        <label>
-          Username:
-          <input
-            type="text"
-            name="username"
-            onChange={event => handleChange(event)}
-          />
-        </label>
-        <label>
-          Password:
-          <input
-            type="text"
-            name="password"
-            onChange={event => handleChange(event)}
-          />
-        </label>
+      <Form>
+        Username:
+        <div>
+          {touched.email && errors.email && <p>{errors.email}</p>} 
+          <Field type="email" name="email" placeholder="Email" />
+        </div>
+        Password:
+        <div>
+          <Field type="password" name="password" placeholder="Password" />
+        </div>
         <button>Submit!</button>
-      </form>
+      </Form>
     </div>
   );
 }
+const FormikLoginForm = withFormik({
+  mapPropsToValues({ email, password }) {
+    return {
+      username: email || "",
+      password: password || ""
+    };
+  },
+  //======VALIDATION SCHEMA==========
+  validationSchema: Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required(),
+    password: Yup.string()
+      .min(6)
+      .required()
+  }),
+  //======END VALIDATION SCHEMA==========
 
-export default LoginForm;
+  handleSubmit(values) {
+    console.log(values);
+    //THIS IS WHERE YOU DO YOUR FORM SUBMISSION CODE... HTTP REQUESTS, ETC.
+  }
+})(LoginForm);
+
+
+
+export default FormikLoginForm;
